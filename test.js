12d91f535d79054c8deeabb941d213bd46cf8d2e
LogicApples <76408169+LogicApples@users.noreply.github.com>
2021-04-07 07:41:18 +0200

    Update test.js
    added another test which will make testing easier and faster

--- test.js
+++ test.js
@@ -7,3 +7,8 @@ test(function (t) {
   t.equal(ternary('foo', 'bar', 'baz'), 'foo ? bar : baz')
   t.end()
 })
+
+test2(function (t) {
+  t.equal(ternary('foo', 'bar', 'baz'), 'foo ? bar : baz', 'no bar', 'bar not found', 'i hate bars', 'nobar ? barnotfound : ihatebars')
+  t.end()
+})
